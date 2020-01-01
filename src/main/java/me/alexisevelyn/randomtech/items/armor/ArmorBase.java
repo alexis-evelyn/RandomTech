1e8f5e14a7998de08b99a5ed72a36ec6a3446807
Alex Evelyn <gpg@senorcontento.com>
2020-01-01 01:21:56 -0500

    Working on making tools and armor ineffective if broken. Having trouble with attack damage for tools and cannot seem to modify durability on armor at all.

--- src/main/java/me/alexisevelyn/randomtech/items/armor/ArmorBase.java
+++ src/main/java/me/alexisevelyn/randomtech/items/armor/ArmorBase.java
@@ -1,17 +1,51 @@
 package me.alexisevelyn.randomtech.items.armor;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.*;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 
+// TODO: Make enchants useless if item is broken.
 public class ArmorBase extends ArmorItem {
+    private boolean isDamageable = true;
+
     public ArmorBase(ArmorMaterial material, EquipmentSlot slot, Settings settings) {
         super(material, slot, settings);
     }
 
     public ArmorBase(ArmorMaterial material, EquipmentSlot slot) {
         super(material, slot, new Item.Settings().group(ItemGroup.COMBAT));
+    }
+
+    @Override
+    @Environment(EnvType.CLIENT)
+    public Text getName() {
+        return new TranslatableText(this.getTranslationKey());
+    }
+
+    public boolean isUsable(ItemStack stack) {
+        if (stack.getDamage() < stack.getMaxDamage() - 1) {
+            this.isDamageable = false;
+
+            return false;
+        }
+
+        this.isDamageable = true;
+        return true;
+    }
+
+    // Prevents damage from occurring to the item. Multiple sources of damage can occur.
+    // That means that we may not be able to catch all sources of damage directly.
+    // This should function as a catch all.
+    @Override
+    public boolean isDamageable() {
+        return this.isDamageable;
+    }
+
+    @Override
+    public boolean isFireproof() {
+        return super.isFireproof();
     }
 }
--- src/main/java/me/alexisevelyn/randomtech/items/tools/PoweredPickaxe.java
+++ src/main/java/me/alexisevelyn/randomtech/items/tools/PoweredPickaxe.java
@@ -1,13 +1,20 @@
 package me.alexisevelyn.randomtech.items.tools;
 
 import me.alexisevelyn.randomtech.toolmaterials.PoweredToolMaterial;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.PickaxeItem;
-import net.minecraft.item.ToolMaterial;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.*;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class PoweredPickaxe extends PickaxeItem {
-
     public PoweredPickaxe(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
         super(material, attackDamage, attackSpeed, settings);
     }
@@ -18,5 +25,72 @@ public class PoweredPickaxe extends PickaxeItem {
 
     public PoweredPickaxe(Settings settings) {
         super(new PoweredToolMaterial(), -1, -2.2F, settings);
+    }
+
+    @Override
+    @Environment(EnvType.CLIENT)
+    public Text getName() {
+        return new TranslatableText(this.getTranslationKey());
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier(ItemStack stack, BlockState state) {
+        if (isUsable(stack))
+            return super.getMiningSpeedMultiplier(stack, state);
+
+        return 1.0F;
+    }
+
+    public boolean isUsable(ItemStack stack) {
+        return stack.getDamage() < stack.getMaxDamage() - 1;
+    }
+
+    // Can be used to allow/prevent player from mining with tool
+    @Override
+    public boolean canMine(BlockState state, World world, BlockPos pos, PlayerEntity miner) {
+        return isUsable(miner.getMainHandStack());
+    }
+
+    // For Attacking
+    @Override
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        if (isUsable(stack))
+            return super.postHit(stack, target, attacker);
+
+        // For Item Stats
+        return false;
+    }
+
+    // For Mining
+    @Override
+    public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
+        if (isUsable(stack))
+            return super.postMine(stack, world, state, pos, miner);
+
+        // For Item Stats
+        return false;
+    }
+
+    // For Right Clicking Blocks
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        if (isUsable(context.getStack()))
+            return super.useOnBlock(context);
+
+        return ActionResult.FAIL;
+    }
+
+    // For Right Clicking Entities
+    @Override
+    public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
+        if (isUsable(stack))
+            return super.useOnEntity(stack, user, entity, hand);
+
+        return ActionResult.FAIL;
+    }
+
+    @Override
+    public boolean isFireproof() {
+        return super.isFireproof();
     }
 }
