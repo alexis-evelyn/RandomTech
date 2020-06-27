40d90114fe1f2aaacfc9a4a4bbffc31d129315f8
Alex Evelyn <gpg@senorcontento.com>
2020-06-27 18:45:24 -0400

    Started Work on Implementing Reborn Core

--- src/main/java/me/alexisevelyn/randomtech/blocks/FirstBlock.java
+++ src/main/java/me/alexisevelyn/randomtech/blocks/FirstBlock.java
@@ -2,15 +2,19 @@ package me.alexisevelyn.randomtech.blocks;
 
 import me.alexisevelyn.randomtech.Materials;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricMaterialBuilder;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
-import net.minecraft.block.Material;
-import net.minecraft.block.MaterialColor;
+import net.minecraft.block.BlockState;
 import net.minecraft.item.ToolMaterials;
 import net.minecraft.sound.BlockSoundGroup;
 
-public class FirstBlock extends Block {
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import reborncore.api.blockentity.IMachineGuiHandler;
+import reborncore.common.blocks.BlockMachineBase;
+import reborncore.common.powerSystem.PowerAcceptorBlockEntity;
+
+public class FirstBlock extends BlockMachineBase {
 
     public FirstBlock() {
         super(FabricBlockSettings
@@ -19,6 +23,21 @@ public class FirstBlock extends Block {
                 .breakByTool(FabricToolTags.PICKAXES, ToolMaterials.IRON.getMiningLevel())
                 .sounds(BlockSoundGroup.NETHERITE)
                 .strength(2.0F, 0.2F));
+    }
+
+    @Override
+    public IMachineGuiHandler getGui() {
+        return null;
+    }
+
+    @Override
+    public boolean hasComparatorOutput(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public int getComparatorOutput(BlockState state, World world, BlockPos pos) {
+        return PowerAcceptorBlockEntity.calculateComparatorOutputFromEnergy(world.getBlockEntity(pos));
     }
 
     // Left here as reference code
