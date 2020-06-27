57085ba0347c8ebda9ef834f40a1a75eb7ccea05
Alex Evelyn <gpg@senorcontento.com>
2020-06-27 16:44:02 -0400

    Fixed Some Bugs

---
+++ src/main/java/me/alexisevelyn/randomtech/Materials.java
@@ -0,0 +1,9 @@
+package me.alexisevelyn.randomtech;
+
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricMaterialBuilder;
+import net.minecraft.block.Material;
+import net.minecraft.block.MaterialColor;
+
+public final class Materials {
+    public static final Material FirstMaterial = new FabricMaterialBuilder(MaterialColor.SPRUCE).build();
+}
--- src/main/java/me/alexisevelyn/randomtech/blocks/FirstBlock.java
+++ src/main/java/me/alexisevelyn/randomtech/blocks/FirstBlock.java
@@ -1,19 +1,22 @@
 package me.alexisevelyn.randomtech.blocks;
 
+import me.alexisevelyn.randomtech.Materials;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricMaterialBuilder;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.block.MaterialColor;
+import net.minecraft.item.ToolMaterials;
 import net.minecraft.sound.BlockSoundGroup;
 
 public class FirstBlock extends Block {
 
-    // Break By Hand Seems to Do Nothing?
     public FirstBlock() {
         super(FabricBlockSettings
-                .of(Material.STONE)
-                .breakByHand(false)
-                .breakByTool(FabricToolTags.PICKAXES)
+                .of(Materials.FirstMaterial)
+                .breakByHand(false).requiresTool()
+                .breakByTool(FabricToolTags.PICKAXES, ToolMaterials.IRON.getMiningLevel())
                 .sounds(BlockSoundGroup.NETHERITE)
                 .strength(2.0F, 0.2F));
     }
