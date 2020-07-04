882d909058a7f321c60c878f1a050fa126434bb8
Alex Evelyn <gpg@senorcontento.com>
2020-07-04 00:52:57 -0400

    Got Working Cross-Dimensional Teleportation

--- src/main/java/me/alexisevelyn/randomtech/blockentities/TeleporterBlockEntity.java
+++ src/main/java/me/alexisevelyn/randomtech/blockentities/TeleporterBlockEntity.java
@@ -2,17 +2,23 @@ package me.alexisevelyn.randomtech.blockentities;
 
 import me.alexisevelyn.randomtech.BlockEntities;
 import me.alexisevelyn.randomtech.Main;
+import net.fabricmc.fabric.api.dimension.v1.EntityPlacer;
+import net.fabricmc.fabric.api.dimension.v1.FabricDimensions;
 import net.minecraft.block.BlockState;
-import net.minecraft.client.util.math.Vector3f;
+import net.minecraft.block.pattern.BlockPattern;
+import net.minecraft.client.util.math.Vector3d;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.world.World;
+import net.minecraft.util.registry.RegistryKey;
 import reborncore.api.IToolDrop;
 import reborncore.api.blockentity.IUpgrade;
 import reborncore.api.blockentity.InventoryProvider;
@@ -22,20 +28,24 @@ import reborncore.client.screen.builder.ScreenHandlerBuilder;
 import reborncore.common.powerSystem.PowerAcceptorBlockEntity;
 import reborncore.common.util.RebornInventory;
 
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
 public class TeleporterBlockEntity extends PowerAcceptorBlockEntity implements IToolDrop, InventoryProvider, BuiltScreenHandlerProvider {
+    // Does Nothing
     int state = 0;
+
+    // Energy Values
     double energyAddend = -100.0;
     double maxPower = 10000;
     double maxInput = 10000;
+
+    // TechReborn's Frequency Transmitter
+    Identifier frequencyTransmitterIdentifier = new Identifier("techreborn", "frequency_transmitter");
+    Optional<Item> frequencyTransmitter = Registry.ITEM.getOrEmpty(frequencyTransmitterIdentifier);
+
+    // Inventory Slot Markers
     RebornInventory<TeleporterBlockEntity> inventory;
-
-    World dimension;
-    Vector3f coordinates = new Vector3f();
-
     int inputSlot = 0;
 
     public TeleporterBlockEntity() {
@@ -134,48 +144,73 @@ public class TeleporterBlockEntity extends PowerAcceptorBlockEntity implements I
         //if (world.getTime() % 20 == 0) {
             //world.setBlockState(pos, world.getBlockState(pos).with(BlockMachineBase.ACTIVE, false));
             //addEnergy(energyAddend);
-
-            updateTeleportLocation();
         //}
+
+        PlayerEntity playerEntity = world.getClosestPlayer(getPos().getX(), getPos().getY(), getPos().getZ(), 2, false);
+
+        if (isPlayerReadyToTeleport(playerEntity))
+            teleportPlayer(playerEntity);
     }
 
-    private void updateTeleportLocation() {
-        // WARNING!!! This Below Code Has VERY LITTLE ERROR CHECKING
-        // -------------------------------------------------
-        // This checks for TechReborn's Frequency Transmitter and Outputs the Destination of the Transmitter
-        // techreborn:frequency_transmitter
-        Identifier frequencyTransmitterIdentifier = new Identifier("techreborn", "frequency_transmitter");
-        Optional<Item> frequencyTransmitter = Registry.ITEM.getOrEmpty(frequencyTransmitterIdentifier);
+    private void teleportPlayer(PlayerEntity playerEntity) {
         ItemStack inputItem = inventory.getStack(inputSlot);
 
-        // TODO: Clean Up Code and Functionify It. Also Add Error Checking For Tags!!!
-        if (frequencyTransmitter.isPresent() && inputItem.getItem().equals(frequencyTransmitter.get())) {
+        if (isTRFrequencyTransmitter(inputItem) || isTeleporterItem(inputItem)) {
             // {"tag": {"pos": {"pos": ["I", 9, 55, 12], "dimension": "minecraft:overworld"}}}
             CompoundTag itemTag = inputItem.getTag();
+
+            if (itemTag == null)
+                return;
 
             CompoundTag root = itemTag.getCompound("pos");
 
             int[] pos = root.getIntArray("pos");
-            String dimension = root.getString("dimension");
-            // dimension
 
-            PlayerEntity playerEntity = Objects.requireNonNull(getWorld()).getClosestPlayer(getPos().getX(), getPos().getY(), getPos().getZ(), 2, false);
-            coordinates.set(pos[0], pos[1], pos[2]);
+            if (pos.length != 3)
+                return;
 
-            if (playerEntity != null && playerEntity.isInSneakingPose() && playerEntity.getBlockPos().equals(this.pos.add(0, 1, 0))) {
-                //System.out.println("Detected: " + playerEntity.getDisplayName().toString());
+            if (playerEntity.getServer() == null) {
+                // TODO: Log fatal error
 
-                playerEntity.teleport(coordinates.getX(), coordinates.getY(), coordinates.getZ());
+                return;
             }
 
-//            System.out.println("Teleporter Destination");
-//            System.out.println("Dimension: " + dimension);
-//
-//            System.out.println("Position X: " + pos[0]);
-//            System.out.println("Position Y: " + pos[1]);
-//            System.out.println("Position Z: " + pos[2]);
+            Identifier dimension = new Identifier(root.getString("dimension"));
+            ServerWorld newWorld = playerEntity.getServer().getWorld(RegistryKey.of(Registry.DIMENSION, dimension));
+
+            if (newWorld == null) {
+                // TODO: Alert player world is not loaded
+
+                return;
+            }
+
+            // The teleport methods by default place the vanilla portal associated with that dimension. Nether - Nether Portal. Overworld - Nether Portal. End - End Portal?
+            // This solves that problem by overriding the vanilla entity placement logic.
+            EntityPlacer entityPlacer = new EntityPlacer() {
+                @Override
+                public BlockPattern.TeleportTarget placeEntity(Entity teleported, ServerWorld destination, Direction portalDir, double horizontalOffset, double verticalOffset) {
+                    return new BlockPattern.TeleportTarget(new Vec3d(pos[0], pos[1], pos[2]), teleported.getVelocity(), (int) teleported.getHeadYaw());
+                }
+            };
+
+            // This is only deprecated because it's experimental.
+            FabricDimensions.teleport(playerEntity, newWorld, entityPlacer);
         }
-        // -------------------------------------------------
+    }
+
+    // This checks for TechReborn's Frequency Transmitter and Outputs the Destination of the Transmitter
+    // techreborn:frequency_transmitter
+    public boolean isTRFrequencyTransmitter(ItemStack item) {
+        return frequencyTransmitter.isPresent() && item.getItem().equals(frequencyTransmitter.get());
+    }
+
+    // This checks for my Teleporter Control Item (used if TechReborn is not installed)
+    public boolean isTeleporterItem(ItemStack item) {
+        return item.getItem().equals(Main.TELEPORTER_CONTROL);
+    }
+
+    public boolean isPlayerReadyToTeleport(PlayerEntity playerEntity) {
+        return playerEntity != null && playerEntity.isInSneakingPose() && playerEntity.getBlockPos().equals(this.pos.add(0, 1, 0));
     }
 
     @Override
