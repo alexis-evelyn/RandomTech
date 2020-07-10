b59a158a6759b4a2081be71615f189086befa3c2
Alex Evelyn <gpg@senorcontento.com>
2020-07-10 18:21:44 -0400

    Get closer to finding what I need. Only managed to block light client side

--- src/main/java/me/alexisevelyn/randomtech/blocks/glass/DarkGlass.java
+++ src/main/java/me/alexisevelyn/randomtech/blocks/glass/DarkGlass.java
@@ -2,10 +2,24 @@ package me.alexisevelyn.randomtech.blocks.glass;
 
 import me.alexisevelyn.randomtech.utility.GenericBlockHelper;
 import me.alexisevelyn.randomtech.utility.Materials;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.AbstractGlassBlock;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
+// TODO: Look at net.minecraft.client.render.block.BlockRenderManager and net.minecraft.client.render.block.BlockModelRenderer
+// TODO: Look for making the block transparent without causing the renderer to allow light through
 public class DarkGlass extends AbstractGlassBlock {
     public DarkGlass() {
         super(FabricBlockSettings
@@ -17,5 +31,25 @@ public class DarkGlass extends AbstractGlassBlock {
                 .suffocates(GenericBlockHelper::never)
                 .blockVision(GenericBlockHelper::never)
                 .strength(0.3F, 0.3F));
+    }
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
+    // Only affects rendering.
+    @Environment(EnvType.CLIENT)
+    public float getAmbientOcclusionLightLevel(BlockState state, BlockView world, BlockPos pos) {
+        return 0.0F;
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!world.isClient) {
+            player.sendMessage(new LiteralText("AO: " + state.getAmbientOcclusionLightLevel(world, pos)), false);
+        }
+
+        return ActionResult.SUCCESS;
     }
 }
