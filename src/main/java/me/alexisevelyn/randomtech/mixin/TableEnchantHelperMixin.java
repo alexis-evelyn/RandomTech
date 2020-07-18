3195823fc65752c5cb42c7c28bf1ab3c5e4683b6
Alex Evelyn <gpg@senorcontento.com>
2020-07-18 15:21:39 -0400

    Tweaked Enchants to Be More Vanilla at Table

--- src/main/java/me/alexisevelyn/randomtech/mixin/TableEnchantHelperMixin.java
+++ src/main/java/me/alexisevelyn/randomtech/mixin/TableEnchantHelperMixin.java
@@ -30,28 +30,35 @@ public abstract class TableEnchantHelperMixin {
 			return;
 
 		Iterator<Enchantment> enchantmentIterator = Registry.ENCHANTMENT.iterator();
-		EnchantmentTarget enchantmentTarget = enchantmentIterator.next().type;
-
-		System.out.println("---");
-		System.out.println("Item: " + new TranslatableText(itemStack.getItem().getTranslationKey()));
-		System.out.println("Item Type: " + enchantmentTarget);
-
-//		System.out.println("Return: ");
-//		for (EnchantmentLevelEntry entry : info.getReturnValue())
-//			System.out.println(entry.enchantment.toString());
-
-		System.out.println("---");
-
 		List<EnchantmentLevelEntry> customAllowedEnchants = Lists.newArrayList();
+
 		while (enchantmentIterator.hasNext()) {
 			Enchantment enchantment = enchantmentIterator.next();
+			int currentLevel = shouldAddEnchantment(itemStack, enchantment, power, treasureAllowed);
 
-			if (CustomEnchantmentHelper.isValidEnchantment(itemStack, enchantment.type) == CustomEnchantmentHelper.ValidEnchant.TRUE)
-				customAllowedEnchants.add(new EnchantmentLevelEntry(enchantment, 1));
+			if (currentLevel != -1)
+				customAllowedEnchants.add(new EnchantmentLevelEntry(enchantment, currentLevel));
 		}
 
 		info.setReturnValue(customAllowedEnchants);
 	}
 
+	private static int shouldAddEnchantment(ItemStack itemStack, Enchantment enchantment, int power, boolean allowTreasure) {
+		if (CustomEnchantmentHelper.isValidEnchantment(itemStack, enchantment.type) != CustomEnchantmentHelper.ValidEnchant.TRUE)
+			return -1;
 
+		if (!enchantment.isAvailableForRandomSelection())
+			return -1;
+
+		if (enchantment.isTreasure() && !allowTreasure)
+			return -1;
+
+		// This checks the enchantment level indirectly based on the input of the power level from the Enchantment table
+		for(int currentLevel = enchantment.getMaxLevel(); currentLevel > enchantment.getMinLevel() - 1; --currentLevel) {
+			if (power >= enchantment.getMinPower(currentLevel) && power <= enchantment.getMaxPower(currentLevel))
+				return currentLevel;
+		}
+
+		return -1;
+	}
 }
