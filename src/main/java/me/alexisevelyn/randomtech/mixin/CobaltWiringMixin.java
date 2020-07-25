631aa24267e7ffc2a86c805f2977638ba569ca49
Alex Evelyn <gpg@senorcontento.com>
2020-07-24 23:23:32 -0400

    So far I've had no such luck getting initial placement to work properly

--- src/main/java/me/alexisevelyn/randomtech/mixin/CobaltWiringMixin.java
+++ src/main/java/me/alexisevelyn/randomtech/mixin/CobaltWiringMixin.java
@@ -14,17 +14,20 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Vector;
-
 @SuppressWarnings("UnusedMixin") // The mixin is used, just is loaded by Fabric and not Sponge methods
 @Mixin(RedstoneWireBlock.class)
-public abstract class CobaltWiringMixin {
+public abstract class CobaltWiringMixin extends Block {
 	/* TODO:
 	 * Get blocks to show up visually correctly when placed down
 	 * Fix bug where a top level piece of wire can power a lower wire of a different type.
 	 */
 
+	public CobaltWiringMixin(Settings settings) {
+		super(settings);
+	}
+
 	@Shadow protected abstract boolean canRunOnTop(BlockView world, BlockPos pos, BlockState floor);
+	@Shadow protected abstract BlockState method_27843(BlockView blockView, BlockState blockState, BlockPos blockPos);
 
 	// The method is currently unmapped (as method_27841), I believe it should be mapped as getRenderConnectionType.
 	@Inject(at = @At("INVOKE"), method = "method_27841(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/Direction;Z)Lnet/minecraft/block/enums/WireConnection;", cancellable = true)
@@ -76,40 +79,62 @@ public abstract class CobaltWiringMixin {
 		boolean isConnectedSouth = currentBlockState.get(RedstoneWireBlock.WIRE_CONNECTION_SOUTH).isConnected();
 		boolean isConnectedEast = currentBlockState.get(RedstoneWireBlock.WIRE_CONNECTION_EAST).isConnected();
 		boolean isConnectedWest = currentBlockState.get(RedstoneWireBlock.WIRE_CONNECTION_WEST).isConnected();
+		boolean isNotConnectedNorthSouth = !isConnectedNorth && !isConnectedSouth;
+		boolean isNotConnectedEastWest = !isConnectedEast && !isConnectedWest;
 
-		Block currentBlock = currentBlockState.getBlock();
+//		boolean isNotConnected = isNotConnected(currentBlockState);
+//		currentBlockState = this.method_27843(world, this.getDefaultState().with(RedstoneWireBlock.POWER, currentBlockState.get(RedstoneWireBlock.POWER)), pos);
+
+//		if (isNotConnected && isNotConnected(currentBlockState)) {
+//			info.setReturnValue(currentBlockState);
+//			return;
+//		}
 
 		Vec3i north = sumVectors(pos, Direction.NORTH.getVector());
 		Vec3i south = sumVectors(pos, Direction.SOUTH.getVector());
 		Vec3i east = sumVectors(pos, Direction.EAST.getVector());
 		Vec3i west = sumVectors(pos, Direction.WEST.getVector());
 
-		Block northBlock = world.getBlockState(convertVectorToBlockPos(north)).getBlock();
-		Block southBlock = world.getBlockState(convertVectorToBlockPos(south)).getBlock();
-		Block eastBlock = world.getBlockState(convertVectorToBlockPos(east)).getBlock();
-		Block westBlock = world.getBlockState(convertVectorToBlockPos(west)).getBlock();
+		BlockState northBlockState = world.getBlockState(convertVectorToBlockPos(north));
+		BlockState southBlockState = world.getBlockState(convertVectorToBlockPos(south));
+		BlockState eastBlockState = world.getBlockState(convertVectorToBlockPos(east));
+		BlockState westBlockState = world.getBlockState(convertVectorToBlockPos(west));
 
-		boolean modifiedState = false;
+//		// This makes it possible to have the wire fully extend to the sides it has nothing to connect to.
+//		if (isNotConnectedEastWest && !isConnectedNorth) {
+//			currentBlockState.with(RedstoneWireBlock.WIRE_CONNECTION_NORTH, WireConnection.SIDE);
+//			modifiedState = true;
+//		}
+//		if (isNotConnectedEastWest && !isConnectedSouth) {
+//			currentBlockState.with(RedstoneWireBlock.WIRE_CONNECTION_SOUTH, WireConnection.SIDE);
+//			modifiedState = true;
+//		}
+//		if (isNotConnectedNorthSouth && !isConnectedEast) {
+//			currentBlockState.with(RedstoneWireBlock.WIRE_CONNECTION_EAST, WireConnection.SIDE);
+//			modifiedState = true;
+//		}
+//		if (isNotConnectedNorthSouth && !isConnectedWest) {
+//			currentBlockState.with(RedstoneWireBlock.WIRE_CONNECTION_WEST, WireConnection.SIDE);
+//			modifiedState = true;
+//		}
 
-		if (isConnectedNorth && isSeparateWire(currentBlock, northBlock)) {
-			currentBlockState = currentBlockState.with(RedstoneWireBlock.WIRE_CONNECTION_NORTH, WireConnection.NONE);
-			modifiedState = true;
+		// This prevents redstone wire and my wire from connecting to each other visually
+		if (isSeparateWire(currentBlockState.getBlock(), northBlockState.getBlock())) {
+			currentBlockState.with(RedstoneWireBlock.WIRE_CONNECTION_NORTH, WireConnection.NONE);
+			info.setReturnValue(currentBlockState);
 		}
-		if (isConnectedSouth && isSeparateWire(currentBlock, southBlock)) {
-			currentBlockState = currentBlockState.with(RedstoneWireBlock.WIRE_CONNECTION_SOUTH, WireConnection.NONE);
-			modifiedState = true;
+		if (isSeparateWire(currentBlockState.getBlock(), southBlockState.getBlock())) {
+			currentBlockState.with(RedstoneWireBlock.WIRE_CONNECTION_SOUTH, WireConnection.NONE);
+			info.setReturnValue(currentBlockState);
 		}
-		if (isConnectedEast && isSeparateWire(currentBlock, eastBlock)) {
-			currentBlockState = currentBlockState.with(RedstoneWireBlock.WIRE_CONNECTION_EAST, WireConnection.NONE);
-			modifiedState = true;
+		if (isSeparateWire(currentBlockState.getBlock(), eastBlockState.getBlock())) {
+			currentBlockState.with(RedstoneWireBlock.WIRE_CONNECTION_EAST, WireConnection.NONE);
+			info.setReturnValue(currentBlockState);
 		}
-		if (isConnectedWest && isSeparateWire(currentBlock, westBlock)) {
-			currentBlockState = currentBlockState.with(RedstoneWireBlock.WIRE_CONNECTION_WEST, WireConnection.NONE);
-			modifiedState = true;
+		if (isSeparateWire(currentBlockState.getBlock(), westBlockState.getBlock())) {
+			currentBlockState.with(RedstoneWireBlock.WIRE_CONNECTION_WEST, WireConnection.NONE);
+			info.setReturnValue(currentBlockState);
 		}
-
-//		if (modifiedState)
-//			info.setReturnValue(currentBlockState);
 	}
 
 	public Vec3i sumVectors(Vec3i vectorOne, Vec3i vectorTwo) {
@@ -134,16 +159,23 @@ public abstract class CobaltWiringMixin {
 		if (blockOne.is(Blocks.REDSTONE_WIRE) && blockTwo.is(RegistryHelper.COBALT_DUST))
 			return true;
 
-		if (blockOne.is(RegistryHelper.COBALT_DUST) && blockTwo.is(Blocks.REDSTONE_WIRE))
-			return true;
-
-		return false;
+		return blockOne.is(RegistryHelper.COBALT_DUST) && blockTwo.is(Blocks.REDSTONE_WIRE);
 	}
 
 	public boolean isSameWire(Block blockOne, Block blockTwo) {
+		// Only check if these are the same wires and nothing else.
+		// If one of these is not a wire, it's false.
+		
 		if (blockOne.is(Blocks.REDSTONE_WIRE) && blockTwo.is(Blocks.REDSTONE_WIRE))
 			return true;
 
 		return blockOne.is(RegistryHelper.COBALT_DUST) && blockTwo.is(RegistryHelper.COBALT_DUST);
+	}
+
+	private static boolean isNotConnected(BlockState state) {
+		return !state.get(RedstoneWireBlock.WIRE_CONNECTION_NORTH).isConnected()
+				&& !state.get(RedstoneWireBlock.WIRE_CONNECTION_SOUTH).isConnected()
+				&& !state.get(RedstoneWireBlock.WIRE_CONNECTION_EAST).isConnected()
+				&& !state.get(RedstoneWireBlock.WIRE_CONNECTION_WEST).isConnected();
 	}
 }
