ff20877cbbfa247716c9b7fea24b49ad2b796416
Alex Evelyn <gpg@senorcontento.com>
2020-07-30 22:14:11 -0400

    Added Cobalt Ore to World Gen

--- src/main/java/me/alexisevelyn/randomtech/blocks/ores/CobaltOre.java
+++ src/main/java/me/alexisevelyn/randomtech/blocks/ores/CobaltOre.java
@@ -3,10 +3,17 @@ package me.alexisevelyn.randomtech.blocks.ores;
 import me.alexisevelyn.randomtech.api.utilities.GenericBlockHelper;
 import me.alexisevelyn.randomtech.api.utilities.MiningLevel;
 import me.alexisevelyn.randomtech.utility.Materials;
+import me.alexisevelyn.randomtech.utility.registryhelpers.main.RegistryHelper;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.decorator.Decorator;
+import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
 
 // TODO: Fix this so that mining level is respected
 public class CobaltOre extends Block {
@@ -21,6 +28,26 @@ public class CobaltOre extends Block {
                 .suffocates(GenericBlockHelper::always)
                 .blockVision(GenericBlockHelper::always)
                 .strength(10.0F, 11.0F));
+    }
+
+    public static void addOreFeature(Biome biome) {
+        // Don't Add Ore to Nether or End
+        if (biome.getCategory() != Biome.Category.NETHER && biome.getCategory() != Biome.Category.THEEND) {
+            biome.addFeature(
+                    GenerationStep.Feature.UNDERGROUND_ORES,
+                    Feature.ORE.configure(
+                            new OreFeatureConfig(
+                                    OreFeatureConfig.Target.NATURAL_STONE,
+                                    RegistryHelper.COBALT_ORE.getDefaultState(),
+                                    8 // Ore vein size
+                            )).createDecoratedFeature(
+                            Decorator.COUNT_RANGE.configure(new RangeDecoratorConfig(
+                                    8, // Number of veins per chunk
+                                    0, // Bottom Offset
+                                    13, // Min y level
+                                    32 // Max y level
+                            ))));
+        }
     }
 }
 
--- src/main/java/me/alexisevelyn/randomtech/utility/registryhelpers/main/PostRegistryHelper.java
+++ src/main/java/me/alexisevelyn/randomtech/utility/registryhelpers/main/PostRegistryHelper.java
@@ -4,18 +4,27 @@ import me.alexisevelyn.randomtech.Main;
 import me.alexisevelyn.randomtech.api.items.armor.generic.GenericPoweredArmor;
 import me.alexisevelyn.randomtech.api.items.tools.generic.GenericPoweredTool;
 import me.alexisevelyn.randomtech.api.utilities.cardinalcomponents.BrokenItemComponent;
+import me.alexisevelyn.randomtech.blocks.ores.CobaltOre;
 import me.alexisevelyn.randomtech.items.armor.powered.PoweredHelmet;
 import nerdhub.cardinal.components.api.ComponentRegistry;
 import nerdhub.cardinal.components.api.ComponentType;
 import nerdhub.cardinal.components.api.event.ItemComponentCallback;
+import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
 import net.fabricmc.fabric.api.network.PacketContext;
 import net.fabricmc.fabric.api.network.ServerSidePacketRegistry;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.decorator.Decorator;
+import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
 
 public class PostRegistryHelper {
     public static final ComponentType<BrokenItemComponent> BROKEN_ITEM_COMPONENT =
@@ -28,6 +37,12 @@ public class PostRegistryHelper {
                 ItemComponentCallback.event(item).register((stack, components) -> components.put(BROKEN_ITEM_COMPONENT, new BrokenItemComponent(stack)));
             }
         });
+
+        // Register Post-Generation in Existing Biomes
+        Registry.BIOME.forEach(this::handleBiome);
+
+        // Register Post-Generation in Future Added Biomes
+        RegistryEntryAddedCallback.event(Registry.BIOME).register((i, identifier, biome) -> handleBiome(biome));
 
         // To Handle Hotkey Presses From Client
         ServerSidePacketRegistry.INSTANCE.register(PreRegistryHelper.keybindPacketIdentifier, (packetContext, attachedData) -> {
@@ -76,5 +91,10 @@ public class PostRegistryHelper {
 
         if (rootTag.getInt("zoom") > 0)
             rootTag.remove("zoom");
+    }
+
+    private void handleBiome(Biome biome) {
+        // Add Structure Features to Biomes Here
+        CobaltOre.addOreFeature(biome);
     }
 }
