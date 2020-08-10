d084f7603bab585b4b6c4c79529631e103119d74
Alex Evelyn <gpg@senorcontento.com>
2020-08-10 17:15:04 -0400

    Working on /kill invulnerability

--- src/main/java/me/alexisevelyn/randomtech/mixin/KillPreventionMixin.java
+++ src/main/java/me/alexisevelyn/randomtech/mixin/KillPreventionMixin.java
@@ -1,20 +1,32 @@
 package me.alexisevelyn.randomtech.mixin;
 
-import net.minecraft.client.gui.screen.TitleScreen;
-import net.minecraft.server.command.KillCommand;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@SuppressWarnings({"UnusedMixin", "EmptyMethod"}) // The mixin is used, just is loaded by Fabric and not Sponge methods
-@Mixin(KillCommand.class)
-public class KillPreventionMixin {
+@SuppressWarnings("UnusedMixin") // The mixin is used, just is loaded by Fabric and not Sponge methods
+@Mixin(PlayerEntity.class)
+public abstract class KillPreventionMixin {
 	// For /kill invulnerability for api
 
-//	@Inject(at = @At("HEAD"), method = "init()V")
-//	private void init(CallbackInfo info) {
-//		// Nothing For Now. Commented Out print line to prevent unnecessary messages in the log
-//		//System.out.println("This line is printed by an example mod mixin!");
-//	}
+    @Shadow public abstract boolean damage(DamageSource source, float amount);
+    // @Shadow public abstract void kill();
+
+    @Inject(at = @At("HEAD"), method = "damage(Lnet/minecraft/entity/damage/DamageSource;F)Z", cancellable = true)
+	private void damage(DamageSource damageSource, float amount, CallbackInfoReturnable<Boolean> info) {
+//        if (damageSource.isOutOfWorld())
+//            info.setReturnValue(false);
+	}
+
+    // @Inject(at = @At("HEAD"), method = "kill()V", cancellable = true)
+	private void kill(CallbackInfo info) {
+        // TODO: Figure out how to overwrite this method for the player only
+    }
 }
