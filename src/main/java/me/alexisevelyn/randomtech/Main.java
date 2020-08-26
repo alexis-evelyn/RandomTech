550fd373a660fd6ce7335916f05ff4b4c25f488c
Alex Evelyn <gpg@senorcontento.com>
2020-08-25 22:17:03 -0400

    And Scene

--- src/main/java/me/alexisevelyn/randomtech/Main.java
+++ src/main/java/me/alexisevelyn/randomtech/Main.java
@@ -41,7 +41,5 @@ public class Main implements ModInitializer {
 		preRegistryHelper.preRegister();
 		registryHelper.register();
 		postRegistryHelper.postRegister();
-
-		me.alexisevelyn.randomtech.api.Main test = new me.alexisevelyn.randomtech.api.Main();
 	}
 }
