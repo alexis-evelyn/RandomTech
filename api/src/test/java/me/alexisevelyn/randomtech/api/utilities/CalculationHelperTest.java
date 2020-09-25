fe123a86ff04bf965e9d136f8a6132d962c7eb87
Alex Evelyn <alexis.a.evelyn@gmail.com>
2020-09-25 17:47:30 -0400

    Updated JUnit and Jetbrains Annotations

--- api/src/test/java/me/alexisevelyn/randomtech/api/utilities/CalculationHelperTest.java
+++ api/src/test/java/me/alexisevelyn/randomtech/api/utilities/CalculationHelperTest.java
@@ -3,8 +3,8 @@ package me.alexisevelyn.randomtech.api.utilities;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3i;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /* Helpful Links Provided By Owner of Repo
  * https://github.com/thinkslynk/refinedstorage/tree/fabric-working/src/test/kotlin/com/refinedmods/refinedstorage/data/sync
@@ -19,13 +19,12 @@ import org.junit.Test;
  * These are sample tests to help me learn how to make unit tests. I'll be learning how to mock objects later after I fix a bunch of bugs.
  */
 public class CalculationHelperTest {
-
 	@Test
 	public void addVectors() {
 		// TODO: Add Null Checks
 		BlockPos result = CalculationHelper.addVectors(new Vec3i(12, -10,0), new Vec3i(-10, 20, 5));
 
-		Assert.assertEquals("Adding Vectors Failed!", new BlockPos(2, 10, 5), result);
+		Assertions.assertEquals(new BlockPos(2, 10, 5), result);
 	}
 
 	@Test
@@ -33,7 +32,7 @@ public class CalculationHelperTest {
 		// TODO: Add Null Checks
 		BlockPos result = CalculationHelper.subtractVectors(new Vec3i(-10, -20,200), new Vec3i(10, -10, -90));
 
-		Assert.assertEquals("Subtracting Vectors Failed!", new BlockPos(-20, -10, 290), result);
+		Assertions.assertEquals(new BlockPos(-20, -10, 290), result);
 	}
 
 	@Test
@@ -41,7 +40,7 @@ public class CalculationHelperTest {
 		// TODO: Add Null Checks
 		BlockPos result = CalculationHelper.multiplyVectors(new Vec3i(3, 4,5), new Vec3i(4, 3, 2));
 
-		Assert.assertEquals("Multiplying Vectors Failed!", new BlockPos(12, 12, 10), result);
+		Assertions.assertEquals(new BlockPos(12, 12, 10), result);
 	}
 
 	@Test
@@ -50,7 +49,7 @@ public class CalculationHelperTest {
 		// TODO: Add Zero Checks
 		BlockPos result = CalculationHelper.divideVectors(new Vec3i(10, 15,9), new Vec3i(10, 3, 3));
 
-		Assert.assertEquals("Dividing Vectors Failed!", new BlockPos(1, 5, 3), result);
+		Assertions.assertEquals(new BlockPos(1, 5, 3), result);
 	}
 
 	@Test
@@ -59,7 +58,7 @@ public class CalculationHelperTest {
 		double result = CalculationHelper.distanceVectors(new Vec3i(3, 5,9), new Vec3i(3, 2, 1));
 		// (0, 3, 8) -> 11
 
-		Assert.assertEquals("Distancing Vectors Failed!", 11, result, 0.0);
+		Assertions.assertEquals(11, result, 0.0);
 	}
 
 	@Test
@@ -68,7 +67,7 @@ public class CalculationHelperTest {
 		// TODO: Verify All Directions
 		Direction result = CalculationHelper.getDirection(Vec3i.ZERO, Direction.NORTH.getVector());
 
-		Assert.assertEquals("Checking Directions Failed!", Direction.NORTH, result);
+		Assertions.assertEquals(Direction.NORTH, result);
 	}
 
 	@Test
@@ -79,6 +78,6 @@ public class CalculationHelperTest {
 
 		double result = CalculationHelper.proportionCalculator(3, 0, 9, 0, 15);
 
-		Assert.assertEquals("Calculating Proportion Failed!", 5, result, 0.0);
+		Assertions.assertEquals(5, result, 0.0);
 	}
 }
